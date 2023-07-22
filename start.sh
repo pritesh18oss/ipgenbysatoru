422b83352ddf9074df5ebf10d70451db5454927e
pritesh18oss <135111100+pritesh18oss@users.noreply.github.com>
2023-07-22 13:23:54 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,7 @@
-a
+echo "Cloning main Repository"
+git clone https://github.com/pritesh18oss/ipgenbysatoru /ipgenbysatoru
+cd /ipgenbysatoru
+echo "installing requirements"
+pip install requirements.txt
+echo "bot starting"
+python bot.py

d5ee37dee2aec1cf7008f6e54d1b07c4a40174c0
pritesh18oss <135111100+pritesh18oss@users.noreply.github.com>
2023-07-22 13:56:30 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-echo "Cloning main Repository"
-git clone https://github.com/pritesh18oss/ipgenbysatoru /ipgenbysatoru
-cd /ipgenbysatoru
-echo "installing requirements"
-pip install requirements.txt
-echo "bot starting"
-python bot.py
